857b0772033ca8836b604e43a73298d150bfe6c3
xdarkphoneix <rajsaurav589@gmail.com>
2024-12-10 15:29:33 +0530

    services file added

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,35 @@
+import axios from "axios"
+
+export class AuthService{
+
+     API_END_POINT="http://localhost:8000/api/v1"
+
+   
+     async createAccount({fullName, email, username, password,avatar}){
+        console.log(fullName);
+        
+        const config={
+            headers:{
+                "Content-Type":"multipart/form-data; boundary=<calculated when request is sent>"
+            }
+        }
+
+      const {data} = await axios.post(`${this.API_END_POINT}/users/register`,{
+          fullName,
+          email,
+          username,
+          password,
+          avatar  
+        },config)
+       
+        console.log(data);
+        
+        return data;
+     }
+   
+}
+
+
+const service=new AuthService()
+
+export default service
